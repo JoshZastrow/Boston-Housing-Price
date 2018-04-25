eb596478a6613c301a7d0e64e8e76b168733f296
Josh Zastrow <j.a.zastrow.jr@gmail.com>
2018-04-25 15:10:57 -0400

    Completed Zappa config with errors
    competed Zappa setup, API POST request does not work. See ZAPPA_ERROR.txt

--- app/api.py
+++ app/api.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, json
 from sklearn.externals import joblib
 import boto3
 import numpy as np
 import pickle
+import dill
 
 app = Flask(__name__)
 
@@ -12,45 +13,6 @@ BUCKET_NAME = 'ml-boston-housing'
 MODEL_FNAME = 'model.pkl'
 
 S3 = boto3.client('s3', region_name='us-east-1')
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/predict', methods=['POST'])
-def make_prediction():
-    if request.method == 'POST':
-        app.logger.info("{} request received from: {}".format(
-            request.method, request.remote_addr))
-
-        body = request.get_json()
-        if type(body) == 'dict':
-            data = body['data']
-            print('\nrequest json output:\n\n', data)
-        else:
-            print('\nrequest json output:\n\n', body)
-
-        mdl = load_model(MODEL_FNAME)
-
-        if not request.json() is None:
-            if 'data' in request.json():
-                body = request.get_json()
-                data = body['data']
-                print('POST RECIEVED JSON DATA:\n\tData Type:{}\n\tData Shape: '.format(
-                    type(data), data.shape))
-                result = np.squeeze(mdl.predict(data))
-                return jsonify({'result': result})
-        else:
-            input_data = np.zeros((1, 13))
-            for i, k in enumerate(request.form.keys()):
-                input_data[0, i] = request.form.get(k, 0)
-
-            result = np.squeeze(mdl.predict(input_data))
-            return render_template('index.html', label=result)
-
 
 # memoized annotation, caches model file after it's pulled
 def memoize(f):
@@ -64,14 +26,47 @@ def memoize(f):
     return helper
 
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+    app.logger.info("{} request received from: {}".format(
+        request.method, request.remote_addr))
+
+    if not request.get_json() is None:
+        if 'data' in request.get_json():
+            body = request.get_json()
+            data = np.array([body['data']])
+
+            result = {'result': predict(data)}
+            return json.dumps(result)
+        else:
+            return json.dumps({'result': 'Error: '
+                               'incoming json does not have data key field'})
+    else:
+        input_data = np.zeros((1, 13))
+        for i, k in enumerate(request.form.keys()):
+            input_data[0, i] = request.form.get(k, 0)
+
+        result = predict(input_data)
+        return render_template('index.html', label=result)
+
+
 @memoize
 def load_model(key):
     response = S3.get_object(Bucket=BUCKET_NAME, Key=key)
-    print('response : ', type(response['Body'].read()))
-    # modelstr = response['Body'].read()
-    # model = pickle.loads(modelstr)
+    modelstr = response['Body'].read()
+    model = dill.loads(modelstr)
 
-    return joblib.load('model.pkl')
+    return model  # joblib.load(key)
+
+def predict(data):
+    mdl = load_model(MODEL_FNAME)
+    return str(np.squeeze(mdl.predict(data)))
 
 
 if __name__ == '__main__':
--- app/model/trainer.py
+++ app/model/trainer.py
@@ -1,6 +1,11 @@
-# coding: utf-8
 import numpy as np
-
+from sklearn.externals import joblib
+from keras.datasets import boston_housing
+from sklearn import preprocessing as p
+from sklearn import model_selection as mdl
+import pickle
+import dill
+import sys
 
 class NeuralNetwork():
     """
@@ -192,3 +197,71 @@ class NeuralNetwork():
 
     def predict(self, X):
         return self.mse_loss(X, drop_p=1, predict=True)
+
+
+if __name__ == "__main__":
+
+    # Get Data
+    (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+
+    # join the x and y together for shuffling
+    y_train = y_train.reshape((y_train.shape[0], 1))
+    data = np.hstack([x_train, y_train])
+
+    # Make random generation repeatable
+    seed = 7
+    np.random.seed(seed)
+
+    # small dataset, shuffling and folding the data
+    shuff_count, i = 3, 0
+    split_count = 4
+    epoch_count = 80
+    b = 0
+
+    # Fit a scaling function to the train data
+    scale = p.StandardScaler().fit(x_train)
+
+    # Initiate First Model
+    best_model = NeuralNetwork(input_dim=x_train.shape[1], units=64)  # base_model()
+
+    while i < shuff_count:
+        i += 1
+        data_folds = mdl.KFold(n_splits=split_count,
+                               shuffle=True,
+                               random_state=seed).split(data)
+
+        for dfold in data_folds:
+            train, valid = dfold
+
+            # Seperate each k fold of train data into a train and validation set
+            xt = scale.transform(data[train, :-1])
+            yt = data[train, -1:]
+
+            # Transform validation set based on scaling on training set
+            xv = scale.transform(data[valid, :-1])
+            yv = data[valid, -1:]
+
+            model = NeuralNetwork(
+                input_dim=x_train.shape[1], units=64)  # base_model()
+
+            history = model.fit(xt, yt, validation_data=(xv, yv), epochs=epoch_count)
+
+            # Evaluate each model, keep the best one
+            curr_evaluation = model.evaluate(
+                scale.transform(x_test), y_test)  # , verbose=0)
+            best_evaluation = best_model.evaluate(
+                scale.transform(x_test), y_test)  # , verbose=0)
+
+            if curr_evaluation['mean_squared_error'] < \
+               best_evaluation['mean_squared_error']:
+                best_model = model
+
+            b += 1
+
+    print('Base Model mean absolute error on test results: $', end='')
+    mae = best_model.evaluate(scale.transform(x_test), y_test[:, None])[
+        'mean_absolute_error']
+    print(round(mae * 1000, 2))
+
+    # Dump Model
+    dill.dump(best_model, open("app/model/model.pkl", "wb"))
